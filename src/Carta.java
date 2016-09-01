ac131d0104406e32d10e823cf55d37c25a05799b
Ana Flores López <Ana Flores Lpez>
2016-09-01 00:23:10 -0500

    Agregé métodos Defensa y Ataque

--- src/Carta.java
+++ src/Carta.java
@@ -1,21 +1,64 @@
-
+/**
+* Clase para generar las cartas del jugador.
+*/
 public class Carta {
+	//El nombre de la carta.
 	private String nombre;
-	private String coste;
-	//private Image imagen;
+	//La imagen que se muestra en la carta.
 	private String imagen;
 	//private List<Habilidad> listaHabilidades;
 	private String descripcion;
 	private int ataque;
 	private int defensa;
+	private int nivel;
 
-	public Carta(String nombre, String coste, String imagen, String descripcion, int ataque, int defensa) {
+	/**
+	* Constructor.
+	*/
+	public Carta(String nombre, String imagen, String descripcion, int nivel, int max_ataque, int min_ataque) {
 		this.nombre = nombre;
-		this.coste = coste;
 		this.imagen = imagen;
 		this.descripcion = descripcion;
-		this.ataque = ataque;
-		this.defensa = defensa;
+		this.nivel = nivel;
+		this.max_ataque = max_ataque;
+		this.min_ataque = min_ataque;
+	}
+
+	/**
+	* Método para generar un ataque de manera aleatoria entre un rango de min_ataque y 
+	* max_ataque, según el nivel de la carta se le asignara un valor bonus al ataque.
+	* @param max_ataque El maximo valor que puede alcanzar un ataque.
+	* @param min_ataque El minimo valor que puede alcanzar un ataque.
+	* @return ataque El valor del ataque.
+	*/
+	protected ataque(int max_ataque, int min_ataque, int nivel) {
+		ataque = ThreadLocalRandom.current().nextInt(min_ataque, max_ataque);
+		if (nivel <= 3){
+			ataque += 1;
+		} else if (nivel > 3 && nivel <= 7){
+			ataque += 2;
+		} else if (nivel > 7){
+			ataque += 3;
+		}
+		return ataque;
+	}
+
+	/**
+	* Método para generar la defensa, esta se asigna según el nivel de la carta,
+	* sin embargo se bajaran puntos de defensa de manera aleatoria entre un rango 
+	*de min_ataque y ataque.
+	*/
+	protected int defensa(int nivel, int ataque, int min_ataque){
+		int aux = ThreadLocalRandom.current().nextInt(min_ataque, ataque);
+		if (nivel <= 2){
+			defensa = 5;
+		} else if (nivel >= 3 && nivel < 6){
+			defensa = 9 ;
+		} else if (nivel >= 7){
+			defensa =;
+		}
+		defensa -= aux;
+		return defensa;
 	}
 
 	public String getNombre(){return nombre;}
